2d444f8c5ac7516996b94fc413cd65ca6bb6b5f8
Feeronus <116346258+Feeronus@users.noreply.github.com>
2023-10-14 02:40:22 +0300

    Update Main.cpp

--- Task1/Main.cpp
+++ Task1/Main.cpp
@@ -17,112 +17,111 @@ int main()
 	int continue_num;
 	while(true)
 	{
-		cout << "Âûáåðèòå âûïîëíÿåìîå çàäàíèå 1-5: ";
+		cout << "Ã‚Ã»Ã¡Ã¥Ã°Ã¨Ã²Ã¥ Ã¢Ã»Ã¯Ã®Ã«Ã­Ã¿Ã¥Ã¬Ã®Ã¥ Ã§Ã Ã¤Ã Ã­Ã¨Ã¥ 1-5: ";
 		cin >> select;
 		switch (select)
 		{
 		case(1):
 		{
-			cout << "Ëàáîðàòîðíàÿ ðàáîòà 2 Ðåïíèêîâ Ì.À. âàðèàíò 17" << endl;
-			cout << "Çàäàíèå 1" << endl;
+			cout << "Ã‹Ã Ã¡Ã®Ã°Ã Ã²Ã®Ã°Ã­Ã Ã¿ Ã°Ã Ã¡Ã®Ã²Ã  2 ÃÃ¥Ã¯Ã­Ã¨ÃªÃ®Ã¢ ÃŒ.Ã€. Ã¢Ã Ã°Ã¨Ã Ã­Ã² 17" << endl;
+			cout << "Ã‡Ã Ã¤Ã Ã­Ã¨Ã¥ 1" << endl;
 			double x;
-			cout << "Ââåäèòå ïàðàìåòð x: ";
+			cout << "Ã‚Ã¢Ã¥Ã¤Ã¨Ã²Ã¥ Ã¯Ã Ã°Ã Ã¬Ã¥Ã²Ã° x: ";
 			cin >> x;
 			cout << "f1 = " << RBPO::Lab2::Variant17::Task1::funcF1(x) << endl;
 			cout << "f2 = " << RBPO::Lab2::Variant17::Task1::funcF2(x) << endl;
 			int n;
-			cout << "Ââåäèòå êîëè÷åñòâî ýëåìåíòîâ ñóììû n: ";
+			cout << "Ã‚Ã¢Ã¥Ã¤Ã¨Ã²Ã¥ ÃªÃ®Ã«Ã¨Ã·Ã¥Ã±Ã²Ã¢Ã® Ã½Ã«Ã¥Ã¬Ã¥Ã­Ã²Ã®Ã¢ Ã±Ã³Ã¬Ã¬Ã» n: ";
 			cin >> n;
 			cout << "f3 = " << RBPO::Lab2::Variant17::Task1::funcF3(n) << endl;
 			double e = 0;
-			cout << "Ââåäèòå òî÷íîñòü e: ";
+			cout << "Ã‚Ã¢Ã¥Ã¤Ã¨Ã²Ã¥ Ã²Ã®Ã·Ã­Ã®Ã±Ã²Ã¼ e: ";
 			cin >> e;
 			cout << "f4 = " << RBPO::Lab2::Variant17::Task1::funcF4(e) << endl;
 			break;
 		}
 		case(2):
 		{
-			cout << "Ëàáîðàòîðíàÿ ðàáîòà 2 Ðåïíèêîâ Ì.À. âàðèàíò 17" << endl;
-			cout << "Çàäàíèå 2" << endl;
+			cout << "Ã‹Ã Ã¡Ã®Ã°Ã Ã²Ã®Ã°Ã­Ã Ã¿ Ã°Ã Ã¡Ã®Ã²Ã  2 ÃÃ¥Ã¯Ã­Ã¨ÃªÃ®Ã¢ ÃŒ.Ã€. Ã¢Ã Ã°Ã¨Ã Ã­Ã² 17" << endl;
+			cout << "Ã‡Ã Ã¤Ã Ã­Ã¨Ã¥ 2" << endl;
 			double x;
-			cout << "Ââåäèòå ïàðàìåòð x: ";
+			cout << "Ã‚Ã¢Ã¥Ã¤Ã¨Ã²Ã¥ Ã¯Ã Ã°Ã Ã¬Ã¥Ã²Ã° x: ";
 			cin >> x;
 			cout << "f1 = " << RBPO::Lab2::Variant17::Task2::funcF1(x) << endl;
 			cout << "f2 = " << RBPO::Lab2::Variant17::Task2::funcF2(x) << endl;
 			int n;
-			cout << "Ââåäèòå êîëè÷åñòâî ýëåìåíòîâ ñóììû n: ";
+			cout << "Ã‚Ã¢Ã¥Ã¤Ã¨Ã²Ã¥ ÃªÃ®Ã«Ã¨Ã·Ã¥Ã±Ã²Ã¢Ã® Ã½Ã«Ã¥Ã¬Ã¥Ã­Ã²Ã®Ã¢ Ã±Ã³Ã¬Ã¬Ã» n: ";
 			cin >> n;
 			cout << "f3 = " << RBPO::Lab2::Variant17::Task2::funcF3(n) << endl;
 			double e = 0;
-			cout << "Ââåäèòå òî÷íîñòü e: ";
+			cout << "Ã‚Ã¢Ã¥Ã¤Ã¨Ã²Ã¥ Ã²Ã®Ã·Ã­Ã®Ã±Ã²Ã¼ e: ";
 			cin >> e;
 			cout << "f4 = " << RBPO::Lab2::Variant17::Task2::funcF4(e) << endl;
 			break;
 		}
 		case(3):
 		{
-			cout << "Ëàáîðàòîðíàÿ ðàáîòà 2 Ðåïíèêîâ Ì.À. âàðèàíò 17" << endl;
-			cout << "Çàäàíèå 3" << endl;
+			cout << "Ã‹Ã Ã¡Ã®Ã°Ã Ã²Ã®Ã°Ã­Ã Ã¿ Ã°Ã Ã¡Ã®Ã²Ã  2 ÃÃ¥Ã¯Ã­Ã¨ÃªÃ®Ã¢ ÃŒ.Ã€. Ã¢Ã Ã°Ã¨Ã Ã­Ã² 17" << endl;
+			cout << "Ã‡Ã Ã¤Ã Ã­Ã¨Ã¥ 3" << endl;
 			double x;
-			cout << "Ââåäèòå ïàðàìåòð x: ";
+			cout << "Ã‚Ã¢Ã¥Ã¤Ã¨Ã²Ã¥ Ã¯Ã Ã°Ã Ã¬Ã¥Ã²Ã° x: ";
 			cin >> x;
 			cout << "f1 = " << RBPO::Lab2::Variant17::Task3::funcF1(x) << endl;
 			cout << "f2 = " << RBPO::Lab2::Variant17::Task3::funcF2(x) << endl;
 			int n;
-			cout << "Ââåäèòå êîëè÷åñòâî ýëåìåíòîâ ñóììû n: ";
+			cout << "Ã‚Ã¢Ã¥Ã¤Ã¨Ã²Ã¥ ÃªÃ®Ã«Ã¨Ã·Ã¥Ã±Ã²Ã¢Ã® Ã½Ã«Ã¥Ã¬Ã¥Ã­Ã²Ã®Ã¢ Ã±Ã³Ã¬Ã¬Ã» n: ";
 			cin >> n;
 			cout << "f3 = " << RBPO::Lab2::Variant17::Task3::funcF3(n) << endl;
 			double e = 0;
-			cout << "Ââåäèòå òî÷íîñòü e: ";
+			cout << "Ã‚Ã¢Ã¥Ã¤Ã¨Ã²Ã¥ Ã²Ã®Ã·Ã­Ã®Ã±Ã²Ã¼ e: ";
 			cin >> e;
 			cout << "f4 = " << RBPO::Lab2::Variant17::Task3::funcF4(e) << endl;
 			break;
 		}
 		case(4):
 		{
-			cout << "Ëàáîðàòîðíàÿ ðàáîòà 2 Ðåïíèêîâ Ì.À. âàðèàíò 17" << endl;
-			cout << "Çàäàíèå 3" << endl;
+			cout << "Ã‹Ã Ã¡Ã®Ã°Ã Ã²Ã®Ã°Ã­Ã Ã¿ Ã°Ã Ã¡Ã®Ã²Ã  2 ÃÃ¥Ã¯Ã­Ã¨ÃªÃ®Ã¢ ÃŒ.Ã€. Ã¢Ã Ã°Ã¨Ã Ã­Ã² 17" << endl;
+			cout << "Ã‡Ã Ã¤Ã Ã­Ã¨Ã¥ 4" << endl;
 			double x;
-			cout << "Ââåäèòå ïàðàìåòð x: ";
+			cout << "Ã‚Ã¢Ã¥Ã¤Ã¨Ã²Ã¥ Ã¯Ã Ã°Ã Ã¬Ã¥Ã²Ã° x: ";
 			cin >> x;
 			cout << "f1 = " << RBPO::Lab2::Variant17::Task4::funcF1(x) << endl;
 			cout << "f2 = " << RBPO::Lab2::Variant17::Task4::funcF2(x) << endl;
 			int n;
-			cout << "Ââåäèòå êîëè÷åñòâî ýëåìåíòîâ ñóììû n: ";
+			cout << "Ã‚Ã¢Ã¥Ã¤Ã¨Ã²Ã¥ ÃªÃ®Ã«Ã¨Ã·Ã¥Ã±Ã²Ã¢Ã® Ã½Ã«Ã¥Ã¬Ã¥Ã­Ã²Ã®Ã¢ Ã±Ã³Ã¬Ã¬Ã» n: ";
 			cin >> n;
 			cout << "f3 = " << RBPO::Lab2::Variant17::Task4::funcF3(n) << endl;
 			double e = 0;
-			cout << "Ââåäèòå òî÷íîñòü e: ";
+			cout << "Ã‚Ã¢Ã¥Ã¤Ã¨Ã²Ã¥ Ã²Ã®Ã·Ã­Ã®Ã±Ã²Ã¼ e: ";
 			cin >> e;
 			cout << "f4 = " << RBPO::Lab2::Variant17::Task4::funcF4(e) << endl;
 			break;
 		}
 		case(5):
 		{
-			cout << "Ëàáîðàòîðíàÿ ðàáîòà 2 Ðåïíèêîâ Ì.À. âàðèàíò 17" << endl;
-			cout << "Çàäàíèå 3" << endl;
+			cout << "Ã‹Ã Ã¡Ã®Ã°Ã Ã²Ã®Ã°Ã­Ã Ã¿ Ã°Ã Ã¡Ã®Ã²Ã  2 ÃÃ¥Ã¯Ã­Ã¨ÃªÃ®Ã¢ ÃŒ.Ã€. Ã¢Ã Ã°Ã¨Ã Ã­Ã² 17" << endl;
+			cout << "Ã‡Ã Ã¤Ã Ã­Ã¨Ã¥ 5" << endl;
 			double x;
-			cout << "Ââåäèòå ïàðàìåòð x: ";
+			cout << "Ã‚Ã¢Ã¥Ã¤Ã¨Ã²Ã¥ Ã¯Ã Ã°Ã Ã¬Ã¥Ã²Ã° x: ";
 			cin >> x;
 			cout << "f1 = " << RBPO::Lab2::Variant17::Task5::funcF1(x) << endl;
 			cout << "f2 = " << RBPO::Lab2::Variant17::Task5::funcF2(x) << endl;
 			int n;
-			cout << "Ââåäèòå êîëè÷åñòâî ýëåìåíòîâ ñóììû n: ";
+			cout << "Ã‚Ã¢Ã¥Ã¤Ã¨Ã²Ã¥ ÃªÃ®Ã«Ã¨Ã·Ã¥Ã±Ã²Ã¢Ã® Ã½Ã«Ã¥Ã¬Ã¥Ã­Ã²Ã®Ã¢ Ã±Ã³Ã¬Ã¬Ã» n: ";
 			cin >> n;
 			cout << "f3 = " << RBPO::Lab2::Variant17::Task5::funcF3(n) << endl;
 			double e = 0;
-			cout << "Ââåäèòå òî÷íîñòü e: ";
+			cout << "Ã‚Ã¢Ã¥Ã¤Ã¨Ã²Ã¥ Ã²Ã®Ã·Ã­Ã®Ã±Ã²Ã¼ e: ";
 			cin >> e;
 			cout << "f4 = " << RBPO::Lab2::Variant17::Task5::funcF4(e) << endl;
 			break;
 		}
 		}
-		cout << "Ïðîäîëæèòü èñïûòàíèÿ? 0/1: ";
+		cout << "ÃÃ°Ã®Ã¤Ã®Ã«Ã¦Ã¨Ã²Ã¼ Ã¨Ã±Ã¯Ã»Ã²Ã Ã­Ã¨Ã¿? 0/1: ";
 		cin >> continue_num;
 		if (continue_num == 0)
 		{
 			break;
-			cout << "Âûêëþ÷àþñü";
 			return 0;
 		}
 	}
